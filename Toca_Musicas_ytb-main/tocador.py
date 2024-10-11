b0e72871866cb68a6da5b43030372e5abd2793c4
Trudyscamp (Eduardo Vynnícius) <eduardo.vynnicius@gmail.com>
2024-10-11 12:05:41 -0300

    Initial commit

---
+++ Toca_Musicas_ytb-main/tocador.py
@@ -0,0 +1,450 @@
+import pyautogui as py
+import time
+
+
+def undertale():
+    time.sleep(0.5)
+    py.hotkey('win' , 'd')
+    py.press('win')
+    py.write('https://youtu.be/M2YcFFSMnzE')
+    py.press('enter')
+    time.sleep(5)
+    
+    indice = 0
+    while indice != 2:
+        py.press('2')
+        time.sleep(0.2)
+        py.press('2')
+        time.sleep(0.2)
+        py.press('9')
+        time.sleep(0.3)
+        py.press('6')
+        
+        time.sleep(0.5)
+
+        py.press('6')
+        time.sleep(0.4)
+        py.press('5')
+        time.sleep(0.4)
+        py.press('4')
+        time.sleep(0.3)
+        py.press('2')
+        time.sleep(0.2)
+        py.press('4')
+        time.sleep(0.2)
+        py.press('5')
+        
+        time.sleep(0.4)
+
+        py.press('1')
+        time.sleep(0.2)
+        py.press('1')
+        time.sleep(0.2)
+        py.press('9')
+        time.sleep(0.3)
+        py.press('6')
+        
+        time.sleep(0.5)
+
+        py.press('6')
+        time.sleep(0.4)
+        py.press('5')
+        time.sleep(0.4)
+        py.press('4')
+        time.sleep(0.3)
+        py.press('2')
+        time.sleep(0.2)
+        py.press('4')
+        time.sleep(0.2)
+        py.press('5')
+        
+        
+        indice +=1
+        time.sleep(0.6)
+    time.sleep(2)
+    py.hotkey('alt', 'f4') 
+def zelda():
+    py.hotkey('win' , 'd')
+    py.press('win')
+    py.write('https://youtu.be/EGgnWO0_szw')
+    py.press('enter')
+    time.sleep(5)
+   
+    # 1 parte
+    
+    py.press('4')
+    time.sleep(0.3)
+    py.press('6')
+    time.sleep(0.3)
+    py.press('7')
+    
+    time.sleep(0.4)
+    
+
+    py.press('4')
+    time.sleep(0.3)
+    py.press('6')
+    time.sleep(0.3)
+    py.press('7')
+    
+    time.sleep(0.4)
+    
+    py.press('4')
+    time.sleep(0.3)
+    py.press('6')
+    time.sleep(0.3)
+    py.press('7')
+    time.sleep(0.4)
+    py.press('0')
+    time.sleep(0.3)
+    py.press('9')
+    
+    time.sleep(1)
+    
+    py.press('7')
+    time.sleep(0.3)
+    py.press('8')
+    time.sleep(0.3)
+    py.press('7')
+    
+    time.sleep(0.4)
+    
+    py.press('5')
+    time.sleep(0.3)
+    py.press('3')
+    
+    time.sleep(1.2)
+
+    py.press('2')
+    time.sleep(0.5)
+    py.press('3')
+    time.sleep(0.3)
+    py.press('5')
+    time.sleep(0.3)
+    py.press('3')
+    time.sleep(1.2)
+    
+    #2 parte
+     
+    py.press('4')
+    time.sleep(0.3)
+    py.press('6')
+    time.sleep(0.3)
+    py.press('7')
+    
+    time.sleep(0.4)
+    
+    py.press('4')
+    time.sleep(0.3)
+    py.press('6')
+    time.sleep(0.3)
+    py.press('7')
+    
+    time.sleep(0.4)
+    
+    py.press('4')
+    time.sleep(0.3)
+    py.press('6')
+    time.sleep(0.3)
+    py.press('7')
+    time.sleep(0.4)
+    py.press('0')
+    time.sleep(0.3)
+    py.press('9')
+    
+    time.sleep(1.2)
+    
+    py.press('7')
+    time.sleep(0.3)
+    py.press('8')
+    time.sleep(0.3)
+    py.press('0')
+    
+    time.sleep(0.5)
+    
+    py.press('7')
+    time.sleep(0.3)
+    py.press('5')
+    
+    time.sleep(1.2)
+
+    py.press('7')
+    time.sleep(0.5)
+    py.press('5')
+    time.sleep(0.3)
+    py.press('2')
+    time.sleep(0.3)
+    py.press('3')
+    time.sleep(2)
+    py.hotkey('alt', 'f4') 
+def interstellar():
+     
+    py.hotkey('win' , 'd')
+    py.press('win')
+    py.write('https://youtu.be/rcf1wd36mjc')
+    py.press('enter')
+    contador = 0
+    time.sleep(5)
+    
+    
+    while contador != 2:
+        time.sleep(0.2)
+        py.press('6')
+        time.sleep(0.4)
+        py.press('0')
+        time.sleep(0.6)
+        py.press('6')
+        time.sleep(0.4)
+        py.press('0')
+        
+        time.sleep(0.8)
+        
+        py.press('7')
+        time.sleep(0.4)
+        py.press('0')
+        time.sleep(0.6)
+        py.press('7')
+        time.sleep(0.4)
+        py.press('0')
+        
+        time.sleep(0.8)
+        
+        py.press('8')
+        time.sleep(0.4)
+        py.press('0')
+        time.sleep(0.6)
+        py.press('8')
+        time.sleep(0.4)
+        py.press('0')
+        
+        time.sleep(0.8)
+        
+        py.press('9')
+        time.sleep(0.4)
+        py.press('0')
+        time.sleep(0.6)
+        py.press('9')
+        time.sleep(0.4)
+        py.press('0')
+        contador +=1
+        time.sleep(1)
+    time.sleep(2)
+    py.hotkey('alt', 'f4') 
+def frank():
+    py.hotkey('win' , 'd')
+    py.press('win')
+    time.sleep(0.5)
+    py.write('https://youtu.be/M2YcFFSMnzE')
+    py.press('enter')
+    time.sleep(8)
+    
+    ###########################################################################
+    ###########################################################################
+    ##########################Parte1###########################################
+    py.press('8')
+    time.sleep(0.8)
+    py.press('7')
+    time.sleep(0.2)
+    py.press('6')
+    time.sleep(0.6)
+    py.press('5')
+    time.sleep(0.2)
+    py.press('4')
+    
+    time.sleep(1)
+    
+    py.press('5')
+    time.sleep(0.2)
+    py.press('6')
+    time.sleep(0.6)
+    py.press('8')
+    time.sleep(0.2)
+    py.press('7')
+    
+    time.sleep(1)
+    
+    py.press('6')
+    time.sleep(0.2)
+    py.press('5')
+    time.sleep(0.6)
+    py.press('4')
+    time.sleep(0.1)
+    py.press('3')
+    time.sleep(2)
+    ###########################################################################
+    ###########################################################################
+    ######################parte2###############################################
+    py.press('6')
+    time.sleep(0.4)
+    py.press('5')
+    time.sleep(0.4)
+    py.press('4')
+    time.sleep(0.4)
+    py.press('3')
+    time.sleep(0.5)
+    py.press('2')
+    
+    time.sleep(1)
+    
+    py.press('3')
+    time.sleep(0.3)
+    py.press('4')
+    time.sleep(0.6)
+    py.press('6')
+    time.sleep(0.3)
+    py.press('5')
+
+    time.sleep(1)
+    
+    py.press('4')
+    time.sleep(0.2)
+    py.press('3')
+    time.sleep(0.6)
+    py.press('2')
+    time.sleep(0.2)
+    py.press('1')
+    time.sleep(2)
+    ###########################################################################
+    ########################parte3#############################################
+    py.press('1')
+    time.sleep(0.1)
+    py.press('2')
+    time.sleep(0.6)
+    py.press('6')
+    time.sleep(0.1)
+    py.press('6')
+    
+    time.sleep(2)
+    
+    py.press('8')
+    time.sleep(0.4)
+    py.press('7')
+    time.sleep(0.4)
+    py.press('5')
+    
+    time.sleep(2)    
+    
+    py.press('1')
+    time.sleep(0.1)
+    py.press('1')
+    time.sleep(0.6)
+    py.press('4')
+    time.sleep(0.1)
+    py.press('4')
+    
+    time.sleep(2)
+    
+    py.press('6')
+    time.sleep(0.4)
+    py.press('5')
+    time.sleep(0.4)
+    py.press('4')
+    time.sleep(0.4)
+    py.press('3')
+    time.sleep(2)
+    #################################################################################################
+    
+     ###########################################################################
+    ###########################################################################
+    ##########################Parte1###########################################
+    py.press('8')
+    time.sleep(0.8)
+    py.press('7')
+    time.sleep(0.2)
+    py.press('6')
+    time.sleep(0.6)
+    py.press('5')
+    time.sleep(0.2)
+    py.press('4')
+    
+    time.sleep(1)
+    
+    py.press('5')
+    time.sleep(0.2)
+    py.press('6')
+    time.sleep(0.6)
+    py.press('8')
+    time.sleep(0.2)
+    py.press('7')
+    
+    time.sleep(1)
+    
+    py.press('6')
+    time.sleep(0.2)
+    py.press('5')
+    time.sleep(0.6)
+    py.press('4')
+    time.sleep(0.1)
+    py.press('3')
+    time.sleep(2)
+    ###########################################################################
+    ###########################################################################
+    ###########################################################################
+    ######################parte2###############################################
+    py.press('6')
+    time.sleep(0.4)
+    py.press('5')
+    time.sleep(0.4)
+    py.press('4')
+    time.sleep(0.4)
+    py.press('3')
+    time.sleep(0.5)
+    py.press('2')
+    
+    time.sleep(1)
+    
+    py.press('3')
+    time.sleep(0.3)
+    py.press('4')
+    time.sleep(0.6)
+    py.press('6')
+    time.sleep(0.3)
+    py.press('5')
+
+    time.sleep(1)
+    
+    py.press('4')
+    time.sleep(0.2)
+    py.press('3')
+    time.sleep(0.6)
+    py.press('2')
+    time.sleep(0.2)
+    py.press('1')
+    time.sleep(2)
+    ###########################################################################
+    ##################parte4###################################################
+    py.press('1')
+    time.sleep(0.1)
+    py.press('2')
+    time.sleep(0.6)
+    py.press('6')
+    time.sleep(0.1)
+    py.press('6')
+    
+    time.sleep(2)
+    
+    py.press('8')
+    time.sleep(0.4)
+    py.press('7')
+    time.sleep(0.4)
+    py.press('5')
+    
+    time.sleep(2)
+    
+    py.press('6')
+    time.sleep(0.1)
+    py.press('6')
+    time.sleep(0.6)
+    py.press('1')
+    time.sleep(0.1)
+    py.press('1')
+    
+    time.sleep(2)
+    
+    py.press('1')
+    time.sleep(0.6)
+    py.press('1')
+    time.sleep(0.1)
+    py.press('1') 
+    time.sleep(2)
+    py.hotkey('alt', 'f4')   
